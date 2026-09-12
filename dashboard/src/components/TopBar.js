07ae67a78fbdfb663a538288be3c2a1a4a2c35fa
Shaaz29 <ahmadshaaz2910@gmail.com>
2026-09-12 10:36:03 +0530

    new price updated

--- dashboard/src/components/TopBar.js
+++ dashboard/src/components/TopBar.js
@@ -8,12 +8,12 @@ const TopBar = () => {
       <div className="indices-container">
         <div className="nifty">
           <p className="index">NIFTY 50</p>
-          <p className="index-points">{100.2} </p>
+          <p className="index-points">{24000} </p>
           <p className="percent"> </p>
         </div>
         <div className="sensex">
           <p className="index">SENSEX</p>
-          <p className="index-points">{100.2}</p>
+          <p className="index-points">{74781}</p>
           <p className="percent"></p>
         </div>
       </div>
